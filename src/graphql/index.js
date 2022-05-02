0915b998a7bac47e440a7249bd89d57b24bd72c7
Oliver <oliverjohnpr2013@gmail.com>
2022-05-02 08:01:30 +0800

    Code refactors

---
+++ src/graphql/index.js
@@ -0,0 +1,28 @@
+import { 
+    ApolloClient, 
+    InMemoryCache,
+    HttpLink,
+    from,
+} from  "@apollo/client";
+
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({graphqlErrors, networkError}) => {
+if(graphqlErrors) {
+    graphqlErrors.forEach(({message, location, path}) => {
+    console.log(`Graphql error ${message}`);
+    });
+}
+
+if(networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const link = from([
+errorLink,
+new HttpLink({uri: "http://localhost:3500/graphql"})
+]);
+  
+export const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link,
+});
